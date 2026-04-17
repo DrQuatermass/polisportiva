dc2d60526bd3fbe43f9dd8d835fc93a701c56892
DrQuatermass <sven.rinaldi@gmail.com>
2026-04-17 19:33:24 +0200

    Add privacy and cookie policy pages

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 from .views import CalendarioView, ChiSiamoView, CiclismoView, ContattiView, HomeView
 
 urlpatterns = [
@@ -7,4 +9,14 @@ urlpatterns = [
     path('calendario/', CalendarioView.as_view(), name='calendario'),
     path('ciclismo/', CiclismoView.as_view(), name='ciclismo'),
     path('contatti/', ContattiView.as_view(), name='contatti'),
+    path(
+        'privacy-policy/',
+        TemplateView.as_view(template_name='home/privacy_policy.html'),
+        name='privacy_policy',
+    ),
+    path(
+        'cookie-policy/',
+        TemplateView.as_view(template_name='home/cookie_policy.html'),
+        name='cookie_policy',
+    ),
 ]
